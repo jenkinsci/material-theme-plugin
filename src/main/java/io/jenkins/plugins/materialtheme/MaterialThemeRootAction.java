f1632014c092292097699749ea9f2a4e8a0126f0
Pawel Piatek <pawelx.piatek@intel.com>
2024-01-30 02:31:48 +0100

    Adding purple

--- src/main/java/io/jenkins/plugins/materialtheme/MaterialThemeRootAction.java
+++ src/main/java/io/jenkins/plugins/materialtheme/MaterialThemeRootAction.java
@@ -19,6 +19,7 @@ import static io.jenkins.plugins.materialtheme.MaterialGreyThemeManagerFactory.M
 import static io.jenkins.plugins.materialtheme.MaterialLightBlueThemeManagerFactory.MATERIAL_LIGHT_BLUE_CSS;
 import static io.jenkins.plugins.materialtheme.MaterialGreenThemeManagerFactory.MATERIAL_GREEN_CSS;
 import static io.jenkins.plugins.materialtheme.MaterialYellowThemeManagerFactory.MATERIAL_YELLOW_CSS;
+import static io.jenkins.plugins.materialtheme.MaterialPurpleThemeManagerFactory.MATERIAL_PURPLE_CSS;
 
 @Extension
 public class MaterialThemeRootAction implements UnprotectedRootAction {
@@ -37,14 +38,24 @@ public class MaterialThemeRootAction implements UnprotectedRootAction {
         return "theme-material";
     }
 
+    private static final String css_files[] = new String[] {
+        BASE_CSS,
+        MATERIAL_INDIGO_CSS,
+        MATERIAL_RED_CSS,
+        MATERIAL_GREY_CSS,
+        MATERIAL_LIGHT_BLUE_CSS,
+        MATERIAL_GREEN_CSS,
+        MATERIAL_YELLOW_CSS,
+        MATERIAL_PURPLE_CSS,
+        CUSTOMISED_CSS
+    };
 
     public void doDynamic(StaplerRequest req, StaplerResponse rsp) throws IOException, ServletException {
         String cssFile = req.getRestOfPath();
         if (cssFile.startsWith("/")) {
             cssFile = cssFile.substring(1);
         }
-        if (!Arrays.asList(BASE_CSS, MATERIAL_INDIGO_CSS, MATERIAL_RED_CSS, MATERIAL_GREY_CSS, MATERIAL_LIGHT_BLUE_CSS, MATERIAL_GREEN_CSS, MATERIAL_YELLOW_CSS, CUSTOMISED_CSS)
-                .contains(cssFile)) {
+	if (!Arrays.asList(css_files).contains(cssFile)) {
             rsp.sendError(404);
             return;
         }
